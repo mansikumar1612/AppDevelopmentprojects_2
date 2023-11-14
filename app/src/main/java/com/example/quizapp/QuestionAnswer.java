7d5df2e2af0938a0e4fb6bf5a2eef54a2d1d587b
mansi <mansikumar1612@gmail.com>
2023-11-15 00:23:49 +0530

    first commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which Company owns the android?",
+            "Which one is not the programming language?",
+            "Who is the Father of Computer?",
+            "which company owns the Apple"
+
+    };
+
+    public static String choices[][] ={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Dennis Ritchie","Bill Gates","Charles Babbage","James Gosling"},
+            {"Google","Apple","Nokia","Samsung"}
+
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Notepad",
+            "Charles Babbage",
+            "Apple"
+    };
+}
